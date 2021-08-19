7616d89051e2478a72565c960e23dc2044a00215
Connie Leung <cleung334@gmail.com>
2021-08-19 08:40:43 +0800

    fix: readme and commitlint config file
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-enum': [2, 'always', ['chore']],
+    'type-enum': [
+      2,
+      'always',
+      ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
   },
 }
