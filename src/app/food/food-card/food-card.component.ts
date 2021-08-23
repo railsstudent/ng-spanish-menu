4e66fe7a7d0247c45d45f3ca13b6852631f83983
Connie Leung <cleung334@gmail.com>
2021-08-23 08:19:22 +0800

    feat: food module and foodcard component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ src/app/food/food-card/food-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-food-card',
+  templateUrl: './food-card.component.html',
+  styleUrls: ['./food-card.component.scss'],
+})
+export class FoodCardComponent {}
---
+++ src/app/food/food.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FoodCardComponent } from './food-card'
+
+@NgModule({
+  declarations: [FoodCardComponent],
+  imports: [CommonModule],
+})
+export class FoodModule {}
