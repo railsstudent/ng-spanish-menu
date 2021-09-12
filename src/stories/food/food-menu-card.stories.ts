9edb6b60046529fe14450535d19a94cca3738d82
Connie Leung <cleung334@gmail.com>
2021-09-12 12:07:34 +0800

    feat: food menu card story
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/stories/food/food-menu-card.stories.ts
+++ src/stories/food/food-menu-card.stories.ts
@@ -1,7 +1,8 @@
 import { moduleMetadata } from '@storybook/angular'
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { action } from '@storybook/addon-actions'
 import { Story, Meta } from '@storybook/angular/types-6-0'
-import { FoodMenuCardComponent } from '@/food'
+import { FoodChoiceComponent, FoodMenuCardComponent, FoodQuestionComponent } from '@/food'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 export default {
@@ -10,14 +11,52 @@ export default {
   decorators: [
     moduleMetadata({
       imports: [ReactiveFormsModule, FormsModule],
+      declarations: [FoodQuestionComponent, FoodChoiceComponent],
     }),
   ],
   argTypes: { onClick: { action: 'clicked' } },
 } as Meta
 
-const Template: Story<FoodMenuCardComponent> = (args: FoodMenuCardComponent) => ({
-  props: args,
+const FoodMenuItemTemplate: Story<FoodMenuCardComponent> = (args: FoodMenuCardComponent) => ({
+  props: {
+    ...args,
+    foodChoiceAdded: action('foodChoiceAdded'),
+  },
+  template: `<app-food-menu-card>
+    <app-food-question [question]="menuItem.question" head></app-food-question>
+    <ng-container *ngFor="let choice of menuItem.choices; index as j; trackBy: choiceTrackByFn" body>
+      <app-food-choice [choice]="choice" (foodChoiceAdded)="foodChoiceAdded($event)"></app-food-choice>
+    </ng-container>
+  </app-food-menu-card>`,
 })
 
-export const Primary = Template.bind({})
-Primary.args = {}
+export const Primary = FoodMenuItemTemplate.bind({})
+Primary.args = {
+  menuItem: {
+    id: '1',
+    question: 'Do you wish to order dessert?',
+    choices: [
+      {
+        id: 'a',
+        name: 'Egg salad',
+        description: 'Egg salad',
+        price: 4.99,
+        available: true,
+      },
+      {
+        id: 'd',
+        name: 'Buffalo Chicken Wings',
+        description: 'Spicy chicken wings',
+        price: 8.99,
+        available: true,
+      },
+      {
+        id: 'b',
+        name: 'Oven Baked Zucchini Chips',
+        description: 'Oven Baked Zucchini Chips',
+        price: 5.99,
+        available: true,
+      },
+    ],
+  },
+}
