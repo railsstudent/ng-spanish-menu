f7b6220484ac100e89d4ee8b5c57695c0235187e
railsstudent <cleung334@gmail.com>
2021-10-03 06:52:30 +0000

    Deploy to gh-pages by angular-cli-ghpages
    Triggered by commit: https://github.com/railsstudent/ng-spanish-menu/commit/9ee18f2a048eba0635185bc7fb18543b89095114

--- assets/functions/menu.ts
+++ assets/functions/menu.ts
@@ -69,15 +69,22 @@ const handler: Handler = async (event) => {
   if (event.httpMethod === 'GET') {
     return {
       statusCode: 200,
-      headers: {
-        /* Required for CORS support to work */
-        'Access-Control-Allow-Origin': '*',
-        /* Required for cookies, authorization headers with HTTPS */
-        'Access-Control-Allow-Credentials': true,
-      },
       body: JSON.stringify(data),
     }
+  } else if (event.httpMethod === 'OPTIONS') {
+    // To enable CORS
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET',
+    }
+    return {
+      statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
+      headers,
+      body: 'This was a preflight call!',
+    }
   }
+
   return {
     statusCode: 500,
     body: `Unsupported http method: ${event.httpMethod}`,
