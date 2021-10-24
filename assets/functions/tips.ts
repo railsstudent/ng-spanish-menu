d76a79be5fe02a002d84d7e8143ef35476ea5393
railsstudent <cleung334@gmail.com>
2021-10-24 04:19:42 +0000

    Deploy to gh-pages by angular-cli-ghpages
    Triggered by commit: https://github.com/railsstudent/ng-spanish-menu/commit/b6c2068d3b8df577d3b7025568811e06a975552b

---
+++ assets/functions/tips.ts
@@ -0,0 +1,20 @@
+import { Handler } from '@netlify/functions'
+
+export const handler: Handler = async (event) => {
+  if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        tips: [0, 5, 7.5, 10, 12.5, 15, 20],
+      }),
+    }
+  }
+
+  return {
+    statusCode: 500,
+    body: `Unsupported http method: ${event.httpMethod}`,
+  }
+}
