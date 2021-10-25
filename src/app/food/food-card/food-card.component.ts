39c98a38df90ca83e59248d044813fe6b37507f8
Connie Leung <cleung334@gmail.com>
2021-10-26 02:31:46 +0800

    refactor: inline template

--- src/app/food/food-card/food-card.component.ts
+++ src/app/food/food-card/food-card.component.ts
@@ -4,7 +4,30 @@ import { OrderedFoodChoice } from '../interfaces'
 
 @Component({
   selector: 'app-food-card',
-  templateUrl: './food-card.component.html',
+  template: `
+    <div class="container">
+      <label name="name" class="item">
+        <span class="field">Name:</span>
+        <span>{{ ordered.name }}</span>
+      </label>
+      <label class="item" name="description">
+        <span class="field">Description:</span>
+        <span>{{ ordered.description }}</span>
+      </label>
+      <label class="item" name="price">
+        <span class="field">Price:</span>
+        <span>{{ ordered.price }}</span>
+      </label>
+      <label class="item" name="quantity">
+        <span class="field">Quantity:</span>
+        <span>{{ ordered.quantity }}</span>
+      </label>
+      <label class="item" name="total">
+        <span class="field">Total:</span>
+        <span>{{ ordered.currency }} {{ total }}</span>
+      </label>
+    </div>
+  `,
   styleUrls: ['./food-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
