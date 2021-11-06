17f9c288caa3fdec8efd2a1cd6d495e6f2bedc05
Connie Leung <cleung334@gmail.com>
2021-11-07 01:55:20 +0800

    chore(deps): install commitizen

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-angular'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
-    ],
-  },
-}
+module.exports = { extends: ['@commitlint/config-conventional'] }
