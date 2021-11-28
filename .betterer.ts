804c4c07e2adc8851abe36518354c5c38fbfe33f
Connie Leung <cleung334@gmail.com>
2021-11-28 14:41:08 +0800

    refactor: remove betterer rules that meet their goals
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- .betterer.ts
+++ .betterer.ts
@@ -2,10 +2,8 @@ import { angular } from '@betterer/angular'
 import { eslint } from '@betterer/eslint'
 
 export default {
-  'no more await in loop': () => eslint({ 'no-await-in-loop': 'error' }).include('src/**/*.ts'),
   'stricter template compilation': () =>
     angular('./tsconfig.json', {
       strictTemplates: true,
     }).include('src/**/*.ts', 'src/**/*.html'),
-  'no more eslint-disable': () => eslint({ 'eslint-comments/no-use': 'error' }).include('src/**/*.ts'),
 }
