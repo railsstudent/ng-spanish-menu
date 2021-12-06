cb489b030119aaa0d3fb75bd76dbdc5c8373b54d
Connie Leung <cleung334@gmail.com>
2021-12-07 01:46:32 +0800

    chore: add betterer test for typescript-eslint/recommended-requiring-type-checking
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/food/validators/fulfill-order-validator.ts
+++ src/app/food/validators/fulfill-order-validator.ts
@@ -1,10 +1,15 @@
-import { AbstractControl, FormControl, ValidationErrors, ValidatorFn } from '@angular/forms'
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
 
 import { FoodService } from '../services'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isFormControl(formValue: any): formValue is number {
+  return formValue !== null && formValue !== undefined && typeof formValue === 'number'
+}
+
 export function fulfillOrderValidator(choiceId: string, foodService: FoodService): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const quantity = (control as FormControl).value || 0
+    const quantity = isFormControl(control.value) ? control.value : 0
     const quantityRemained = foodService.getQuantity(choiceId)
     const delta = quantityRemained - quantity
     return delta >= 0
