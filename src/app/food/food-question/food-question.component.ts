4ae4556fcb734f98cd98fb64e66c32464dc78f03
Connie Leung <cleung334@gmail.com>
2021-12-14 08:43:45 +0800

    feat: use tailwind css of food question component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/food/food-question/food-question.component.ts
+++ src/app/food/food-question/food-question.component.ts
@@ -3,7 +3,13 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 @Component({
   selector: 'app-food-question',
   templateUrl: './food-question.component.html',
-  styleUrls: ['./food-question.component.scss'],
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FoodQuestionComponent {
