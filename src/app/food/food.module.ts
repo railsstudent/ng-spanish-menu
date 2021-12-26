4ec80ed4c43c2afc120d09d99cfeb0fdc77b3985
Connie Leung <cleung334@gmail.com>
2021-12-26 16:45:20 +0800

    refactor: food module
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/food/food.module.ts
+++ src/app/food/food.module.ts
@@ -1,34 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { FoodCardModule } from './food-card'
-import { FoodChoiceModule } from './food-choice'
-import { FoodChoiceFormModule } from './food-choice-form'
-import { FoodMenuModule } from './food-menu'
-import { FoodMenuCardModule } from './food-menu-card'
-import { FoodMenuOptionsModule } from './food-menu-options'
-import { FoodQuestionModule } from './food-question'
 import { FoodRoutingModule } from './food-routing.module'
 import { FoodShellModule } from './food-shell'
-import { FoodTotalModule } from './food-total'
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FoodRoutingModule,
-    FoodTotalModule,
-    FoodCardModule,
-    FoodMenuOptionsModule,
-    FoodQuestionModule,
-    FoodChoiceFormModule,
-    FoodChoiceModule,
-    FoodMenuCardModule,
-    FoodMenuModule,
-    FoodShellModule,
-  ],
+  imports: [CommonModule, FoodRoutingModule, FoodShellModule],
 })
 export class FoodModule {}
