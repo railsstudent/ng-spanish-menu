efbd0aed0f0c4a8e6f323f9fda1200572f0c6ff5
Connie Leung <cleung334@gmail.com>
2022-02-27 20:54:38 +0800

    chore(deps): upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,9 +6,11 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@storybook/addon-postcss",
     "@storybook/addon-a11y"
   ],
+  "framework": "@storybook/angular",
   "core": {
     "builder": "webpack5"
   }
