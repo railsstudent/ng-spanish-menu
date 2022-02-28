322f8c4790a143c0678339fdd2e834e44debdf82
Connie Leung <cleung334@gmail.com>
2022-02-28 09:29:14 +0800

    chore: disable adds-on doc as workaround to storybook/angular bugs

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        docs: false,
+      }
+    },
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
     "@storybook/addon-a11y"
